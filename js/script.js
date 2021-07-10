4b72643452d099ab977807bbe0edeff1b9938650
neovasi <novasi.team@gmail.com>
2021-07-10 19:14:45 +0700

    baru

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+// back to top
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
